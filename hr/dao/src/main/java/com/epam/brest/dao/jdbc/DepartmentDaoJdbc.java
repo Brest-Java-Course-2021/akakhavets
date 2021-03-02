16f619bec6c754b79fa57983d00d4a872561433f
Alexey <57506563+Aleks-ir@users.noreply.github.com>
2021-03-03 00:56:11 +0300

    1st dao hr app

--- hr/dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbc.java
+++ hr/dao/src/main/java/com/epam/brest/dao/jdbc/DepartmentDaoJdbc.java
@@ -2,12 +2,36 @@ package com.epam.brest.dao.jdbc;
 
 import com.epam.brest.dao.DepartmentDao;
 import com.epam.brest.model.Department;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DepartmentDaoJdbc implements DepartmentDao {
+
+    private static final String SQL_GET_ALL_DEPARTMENTS = "SELECT D.DEPARTMENT_ID, D.DEPARTMENT_NAME FROM DEPARTMENT AS D ORDER BY D.DEPARTMENT_NAME";
+
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public DepartmentDaoJdbc(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
     @Override
     public List<Department> findAll() {
-        return null;
+        return namedParameterJdbcTemplate.query(SQL_GET_ALL_DEPARTMENTS, new DepartmentRowMapper());
+    }
+
+    private class DepartmentRowMapper implements RowMapper<Department> {
+
+        @Override
+        public Department mapRow(ResultSet resultSet, int i) throws SQLException {
+            Department department = new Department();
+            department.setDepartmentId(resultSet.getInt("DEPARTMENT_ID"));
+            department.setDepartmentName(resultSet.getString("DEPARTMENT_NAME"));
+            return department;
+        }
     }
 }
